05b305a186309ff4ee3bccdbf9623f8bb534d1f3
Alex Pine <pinesol@pinesol-macbookpro.roam.corp.google.com>
2015-06-12 09:45:53 -0400

    start of lisp question

---
+++ hw1/test.sh
@@ -0,0 +1,13 @@
+# Usage test.sh ${name}
+# Assumes ${name}.l, ${name}.y, ${name}_test.txt exists
+
+bison -d ${1}.y && flex ${1}.l && g++ lex.yy.c ${1}.tab.c -o ${1}_parser
+
+while read line; do
+    echo $line | ./${1}_parser
+    if [[ $? -ne 0 ]]; then
+      exit 1
+    fi
+done <${1}_test.txt
+
+echo "Tests pass!"
